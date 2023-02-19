89b8f23b9b6747c299b5d41ab3fd9aef451bddd5
pajowu <git@ca.pajowu.de>
2023-02-20 00:21:04 +0100

    🐛 Convert event description to str before markdownifying
    From django-i18nfield we get a LazyI18nString, which markdownify doesn't unterstand. It crashes instead :)

--- presign/base/templatetags/presign_tags.py
+++ presign/base/templatetags/presign_tags.py
@@ -8,3 +8,8 @@ def can_change_event(context, event: Event):
     if not context["request"].user.is_authenticated:
         return False
     return context["request"].user.has_event_permission(event)
+
+
+@register.filter
+def to_string(value):
+    return str(value)
