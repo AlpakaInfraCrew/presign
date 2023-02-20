5494f5f23a0905ffef0afad56f9d6e985e7bd5d7
pajowu <git@ca.pajowu.de>
2023-02-21 00:21:22 +0100

    ♻️ Tests: Split general from control code

--- presign/conftest.py
+++ presign/conftest.py
@@ -1,9 +1,47 @@
 import os
 
+from django.urls import reverse
+
 import pytest
+from playwright.sync_api import BrowserContext
 
 # Needed for playwright
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
+
+@pytest.fixture
+def live_server_context(context: BrowserContext, live_server):
+    """
+    VERY HACKY! If you have a better idea how to solve this, please tell me :pleading-face:
+
+    This replaces the `new_page` function on the context with one that returns a patched page.
+
+    The patched page has a new `goto_url` function, which uses djangos `reverse` function to access
+    views by name instead of url. This also captures the live_server into the context
+
+    This allows you to do something like
+
+    ```
+    def test_something(live_server_context):
+        page = live_server_context.new_page()
+        page.goto_url("control:index")
+    ```
+    """
+
+    def new_page_func(*args, **kwargs):
+        page = context._hacky_original_new_page(*args, **kwargs)
+
+        def goto_url_func(url, kwargs=None):
+            path = reverse(url, kwargs=kwargs)
+            page.goto(url=f"{live_server.url}{path}")
+
+        page.goto_url = goto_url_func
+        return page
+
+    context._hacky_original_new_page = context.new_page
+    context.new_page = new_page_func
+
+    return context
 
 
 @pytest.fixture
