52821ae4d06ac0a532902c7a33b38ec65280c067
pajowu <git@ca.pajowu.de>
2023-02-20 19:54:04 +0100

    🔧 Switch to django-configurations

--- presign/asgi.py
+++ presign/asgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from configurations.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "presign.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
 application = get_asgi_application()
--- presign/wsgi.py
+++ presign/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "presign.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
 application = get_wsgi_application()
