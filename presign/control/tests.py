4d7f677ca9f1f6d9a17db6a3c26aaf2a5852bd5a
pajowu <git@ca.pajowu.de>
2023-02-21 00:22:56 +0100

    ✅ Add test for creating organizer

--- presign/control/tests.py
+++ presign/control/tests.py
@@ -1,9 +1,11 @@
 from urllib.parse import urlparse
 
-from django.urls import resolve
+from django.urls import resolve, reverse
 
 import pytest
 from playwright.sync_api import BrowserContext
+
+from presign.base.models import Organizer
 
 
 @pytest.fixture
@@ -33,3 +35,32 @@ def test_login_works(
     page.goto_url("control:index")
     resolved_url = resolve(urlparse(page.url)[2])
     assert resolved_url.url_name != "login"
+
+
+@pytest.mark.django_db
+def test_create_organizer(
+    logged_in_context: BrowserContext,
+):
+    page = logged_in_context.new_page()
+    page.goto_url("control:index")
+
+    page.click("text=New Organizer")
+
+    page.fill('input[name^="name_"][lang="de"]', "Test Organizer German")
+    page.fill('input[name^="name_"][lang="en"]', "Test Organizer English")
+    page.fill('[placeholder="Slug"]', "test-organizer")
+
+    page.click("text=Submit")
+
+    assert urlparse(page.url)[2] == reverse(
+        "control:organizer", kwargs={"organizer": "test-organizer"}
+    )
+
+    assert Organizer.objects.filter(slug="test-organizer").count() == 1
+
+    organizer = Organizer.objects.get(slug="test-organizer")
+
+    assert organizer.name.data == {
+        "de": "Test Organizer German",
+        "en": "Test Organizer English",
+    }
