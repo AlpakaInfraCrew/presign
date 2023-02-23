0a4581c778f4bd10806997d24528aa74e21d7765
pajowu <git@ca.pajowu.de>
2023-02-23 22:26:12 +0100

    ♻️ Create hacky browser context patching with proper proxy

--- presign/conftest.py
+++ presign/conftest.py
@@ -12,50 +12,45 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 PLAYWRIGHT_FIXTURES = ["browser", "live_server_context"]
 
 
+class Proxy:
+    def __init__(self, target) -> None:
+        self.proxied_object = target
+
+    def __getattr__(self, name):
+        return getattr(self.proxied_object, name)
+
+
+class LiveServerPage(Proxy):
+    def __init__(self, *args, live_server, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__live_server = live_server
+
+    def get_resolved_url(self):
+        return resolve(self.get_url_path())
+
+    def get_url_path(self):
+        return urlparse(self.url)[2]
+
+    def goto_url(self, url, kwargs=None):
+        path = reverse(url, kwargs=kwargs)
+        return self.goto(url=f"{self.__live_server.url}{path}")
+
+
+class LiveServerBrowserContext(Proxy):
+    def __init__(self, *args, live_server, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__live_server = live_server
+
+    def new_page(self, *args, **kwargs):
+        page = self.proxied_object.new_page(*args, **kwargs)
+        return LiveServerPage(page, live_server=self.__live_server)
+
+
 @pytest.fixture
 def live_server_context(context: BrowserContext, live_server):
-    """
-    VERY HACKY! If you have a better idea how to solve this, please tell me :pleading-face:
-
-    This replaces the `new_page` function on the context with one that returns a patched page.
-
-    The patched page has a new `goto_url` function, which uses djangos `reverse` function to access
-    views by name instead of url. This also captures the live_server into the context
-
-    This allows you to do something like
-
-    ```
-    def test_something(live_server_context):
-        page = live_server_context.new_page()
-        page.goto_url("control:index")
-    ```
-    """
-
-    def new_page_func(*args, **kwargs):
-        page = context._hacky_original_new_page(*args, **kwargs)
-
-        def goto_url_func(url, kwargs=None):
-            path = reverse(url, kwargs=kwargs)
-            page.goto(url=f"{live_server.url}{path}")
-
-        def get_resolved_url():
-            return resolve(page.get_url_path())
-
-        def get_url_path():
-            return urlparse(page.url)[2]
-
-        page.get_resolved_url = get_resolved_url
-        page.get_url_path = get_url_path
-
-        page.goto_url = goto_url_func
-        return page
-
-    context._hacky_original_new_page = context.new_page
-    context.new_page = new_page_func
-
-    context.set_default_timeout(5000)
-
-    return context
+    live_context = LiveServerBrowserContext(context, live_server=live_server)
+    live_context.set_default_timeout(5000)
+    return live_context
 
 
 @pytest.fixture(scope="session")
