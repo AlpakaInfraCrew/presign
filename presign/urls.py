b66dfbe012948ac5c504bb0fd2b83e59e22ebe52
pajowu <git@ca.pajowu.de>
2023-03-08 23:43:49 +0100

    ✨ Add option to serve static & media via django

--- presign/urls.py
+++ presign/urls.py
@@ -1,22 +1,18 @@
-"""presign URL Configuration
+import re
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
+
+
+def get_status_patterns(prefix, view=serve, **kwargs):
+    return [
+        re_path(
+            r"^%s(?P<path>.*)$" % re.escape(prefix.lstrip("/")), view, kwargs=kwargs
+        ),
+    ]
+
 
 urlpatterns = [
     path(settings.ADMIN_URL_BASE, admin.site.urls),
@@ -32,8 +28,15 @@ urlpatterns = [
     ),
 ]
 
+if settings.SELF_SERVE_STATIC:
+    urlpatterns += get_status_patterns(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += get_status_patterns(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
