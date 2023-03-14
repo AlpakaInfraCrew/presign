6e2b8d0cfc821d317a7e93aa80c7983b2dab4147
pajowu <git@ca.pajowu.de>
2023-03-14 20:07:27 +0100

    🐛 Collectstatic in run.sh

--- image/run.sh
+++ image/run.sh
@@ -7,6 +7,8 @@ export DJANGO_CONFIGURATION=Production
 
 python manage.py migrate
 
+python manage.py collectstatic --noinput
+
 if [ ! -z "$PRESIGN_SUPERUSER_NAME"  ] && [ ! -z "$PRESIGN_SUPERUSER_PASS" ]; then
     python manage.py create_superuser_if_not_exists --user "$PRESIGN_SUPERUSER_NAME" --pass "$PRESIGN_SUPERUSER_PASS"
 fi
