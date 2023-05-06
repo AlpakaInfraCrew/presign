7ff5d6b30de7672bc9c76652c9d43e4900bd2dca
pajowu <git@ca.pajowu.de>
2023-05-06 16:04:43 +0200

    ✨ Update to bs5.3-a3, enable dark mode

--- presign/settings/dev.py
+++ presign/settings/dev.py
@@ -9,7 +9,7 @@ class Dev(Base):
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = "django-insecure-*@rchc%vvw5#!((4s1x1=rzh0_myd0_=^=dnzpw^(!3cy#y#3l"
     # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = False
+    DEBUG = True
 
     # Database
     # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
