35e29be14ba4a198ce425aec6a46d6e609a577c3
pajowu <git@ca.pajowu.de>
2023-09-30 16:39:43 +0200

    ✨ Add participant event log

--- presign/control/apps.py
+++ presign/control/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ControlConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "presign.control"
+
+    def ready(self):
+        from . import logdisplay  # noqa
